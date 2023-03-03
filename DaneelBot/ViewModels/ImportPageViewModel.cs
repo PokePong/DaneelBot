22bd51881e9f25ebd701dbbb2b4ba807e1f08e2a
PokePong <loic.besse98@gmail.com>
2023-03-03 23:01:51 +0100

    Begin import page

--- DaneelBot/ViewModels/ImportPageViewModel.cs
+++ DaneelBot/ViewModels/ImportPageViewModel.cs
@@ -1,8 +1,30 @@
-﻿using ReactiveUI;
+﻿using System.Collections.ObjectModel;
+using DynamicData.Binding;
+using ReactiveUI;
 
 namespace DaneelBot.ViewModels;
 
 public class ImportPageViewModel : PageViewModel {
-    
-    public ImportPageViewModel(IScreen screen, int index) : base(screen, index) { }
+    private ObservableCollection<string> _exchangesCollection = new ObservableCollection<string>();
+
+    public ObservableCollection<string> ExchangesCollection {
+        get => _exchangesCollection;
+        set => this.RaiseAndSetIfChanged(ref _exchangesCollection, value);
+    }
+
+    private ObservableCollection<string> _symbolsCollection = new ObservableCollection<string>();
+
+    public ObservableCollection<string> SymbolsCollection {
+        get => _symbolsCollection;
+        set => this.RaiseAndSetIfChanged(ref _symbolsCollection, value);
+    }
+
+    public ImportPageViewModel(IScreen screen, int index) : base(screen, index) {
+        ExchangesCollection.Add("Binance");
+        SymbolsCollection.Add("BTCUSDT");
+        SymbolsCollection.Add("ETHUSDT");
+        SymbolsCollection.Add("ADAUSDT");
+        SymbolsCollection.Add("BNBUSDT");
+        SymbolsCollection.Add("BTCBNB");
+    }
 }
